d4f82e951cd40f916a94d3ae283b2c2eacb79d1d
lucaspedronet <lucaspedrolbg@gmail.com>
2018-11-26 05:41:17 -0300

    TextInput criado para page de messege no enviao da menssagens

--- src/pages/HomeTab/views/Messages/MessageScreen.js
+++ src/pages/HomeTab/views/Messages/MessageScreen.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 import styles from './styles'
 
 class MessageScreen extends Component {
   static navigationOptions = ({ navigation: { state: { params: { restaurante }}} }) => ({
     title: `${restaurante.title}`,
+    headerBackTitle: 'Funcionou',
     headerRight: (
       <TouchableOpacity>
         {/* <Icon type="icon" name="linkedin" style={{ marginRight: 20 }} size={20} /> */}
         <Image
           source={{ uri: restaurante.avatar_url }}
           style={{ width: 40, height: 40, borderRadius: 20, marginRight: 20 }}
-          />
+        />
       </TouchableOpacity>
     )
   })
@@ -24,12 +33,50 @@ class MessageScreen extends Component {
     }).isRequired,
   }
 
+  state = {
+    message: '',
+    batepapo: []
+  }
+
+  enviar() {
+    const { message, batepapo } = this.state;
+    batepapo.push(message)
+    return (
+      <Text> {batepapo} </Text>
+    )
+  }
+
   render() {
     // const { restaurante } = this.props.navigation.state.params;
     console.tron.log(this.props)
     return (
       <View style={styles.container} >
-        <Text> MessageScreen </Text>
+        <ScrollView style={styles.patepapo} >
+          {this.enviar()}
+        </ScrollView>
+
+        <View style={styles.inputContainer}>
+          <TouchableOpacity style={styles.iconButton} >
+            <Icon name="plus" size={20} />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            autoCorrect={false}
+            autoCapitalize="none"
+            multiline
+            placeholderTextColor="#333"
+            underlineColorAndroid="transparent"
+            placeholder="Digite sua dúvida"
+            value={this.state.message}
+            onChangeText={message => this.setState({ message })}
+            returnKeyType="send"
+            onSubmitEditing={this.enviar}
+            keyboardType="name-phone-pad"
+          />
+          <TouchableOpacity style={styles.iconButton} >
+            <Icon name="send" size={20} />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
--- src/pages/HomeTab/views/Messages/styles.js
+++ src/pages/HomeTab/views/Messages/styles.js
@@ -4,17 +4,27 @@ import { colors, metrics } from 'styles'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: colors.white,
+    backgroundColor: colors.white,
   },
-  thumbnail: {
-    width: '100%',
-    height: 142,
-  },
-  thumbnailContainer: {
+  patepapo: {
     flex: 1,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    backgroundColor: colors.darkTransparent,  //black transparent
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.lighter,
+    height: 60,
+  },
+  input: {
+    height: 40,
+    width: 280,
+    borderRadius: 20,
+    backgroundColor: colors.white,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.light,
+    padding: 10,
+    marginHorizontal: 10,
   },
 })
 
